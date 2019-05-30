329cc89fce669ef4ea817da75733ecf5047bfcc8
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-30 07:42:36 -0600

    Create years component and route to years on nav click

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import { fetchData } from "../../api/apiCalls";
 
 class MainPage extends Component {
-  // componentDidMount() {
-  //   this.fetchPhishData();
-  // }
+  componentDidMount() {
+    this.fetchPhishData();
+  }
 
-  // fetchPhishData = () => {
-  //   fetchData(
-  //     `https://cors-anywhere.herokuapp.com/http://phish.in/api/v1/years`
-  //   ).then(response => console.log(response));
-  // };
+  fetchPhishData = () => {
+    fetchData(
+      `https://cors-anywhere.herokuapp.com/http://phish.in/api/v1/years?include_show_counts=true`
+    ).then(response => console.log(response.data));
+  };
 
   render() {
+    const currentPath = this.props.location.pathname;
+    console.log(currentPath);
     return (
       <div className="main-page">
         <h1>Main Page</h1>
---
+++ src/Components/Years/Years.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './Years.scss';
+
+class Years extends Component {
+  render() {
+    return (
+      <div className="years-component">
+        <h1>YEARS</h1>
+      </div>
+    )
+  }
+}
+
+export default Years;
