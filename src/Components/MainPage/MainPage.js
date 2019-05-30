d08fc2052fa3e9bd1dc152049fe1a473aaa9a636
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-29 20:09:24 -0600

    Initial file and router setup

---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+// import { fetchData } from "../../api/apiCalls";
+
+class MainPage extends Component {
+  // componentDidMount() {
+  //   this.fetchPropertyData();
+  // }
+
+  // fetchPropertyData = () => {
+  //   fetchData(
+  //     `https://cors-anywhere.herokuapp.com/http://phish.in/api/v1/years`
+  //   ).then(response => console.log(response));
+  // };
+
+  render() {
+    return (
+      <div className="main-page">
+        <h1>Main Page</h1>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
