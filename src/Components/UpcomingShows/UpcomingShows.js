512f9c96ad62b2ae8037ded954b4215ce854dba6
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-06-01 21:34:33 -0600

    Render upcoming shows to DOM

---
+++ src/Components/UpcomingShows/UpcomingShows.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class UpcomingShows extends Component {
+  render() {
+    return (
+      <div className="upcoming-shows-component">
+      <h1>Upcoming!</h1>
+      </div>
+    )
+  }
+}
+
+export default UpcomingShows;
