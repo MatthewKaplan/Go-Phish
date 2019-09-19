09e6686e5f3110425b3f4886eabdedb176654317
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-09-19 02:14:58 -0600

    Sidebar navigation for mobile view

--- src/Containers/NavBar/NavBar.js
+++ src/Containers/NavBar/NavBar.js
@@ -14,9 +14,16 @@ import { cleanTours } from "../../Helpers/cleaners";
 import PropTypes from "prop-types";
 
 export class NavBar extends Component {
-  state = { subNav: false };
+  state = { subNav: false, sidebarActive: false };
+
+  toggleSidebar = () => {
+    this.setState({
+      sidebarActive: !this.state.sidebarActive
+    });
+  };
 
   fetchYears = async () => {
+    this.toggleSidebar();
     this.toggleSubNav(false);
     if (this.props.years.length === 0) {
       this.props.loadingData(true);
@@ -31,6 +38,7 @@ export class NavBar extends Component {
   };
 
   fetchTours = async () => {
+    this.toggleSidebar();
     this.toggleSubNav(false);
     if (this.props.tours.length === 0) {
       this.props.loadingData(true);
@@ -47,6 +55,7 @@ export class NavBar extends Component {
   };
 
   fetchPhishData = async () => {
+    this.toggleSidebar();
     this.toggleSubNav(false);
     if (this.props.members.length === 0) {
       const url = `https://cors-anywhere.herokuapp.com/https://peaceful-castle-66511.herokuapp.com/api/v1/phish/members`;
@@ -62,96 +71,191 @@ export class NavBar extends Component {
   };
 
   toggleSubNav = bool => {
+    this.toggleSidebar();
     this.setState({
       subNav: bool
     });
   };
 
   render() {
+    const { sidebarActive } = this.state;
     return (
-      <header>
-        <div className="container">
-          <nav className="main-nav">
-            <Link
-              to="/"
-              className="link logo-link"
-              data-test="logo-btn"
-              onClick={() => this.toggleSubNav(false)}
-            >
-              <h1 className="logo">
-                G<span>O</span>PH<span>I</span>SH
-              </h1>
-            </Link>
-            <ul>
-              <li>
-                <NavLink
-                  to="/BandBio"
-                  activeClassName="active"
-                  className="link"
-                  data-test="band-btn"
-                  onClick={() => this.fetchPhishData()}
-                >
-                  Band
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/Years"
-                  activeClassName="active"
-                  className="link"
-                  data-test="years-btn"
-                  onClick={() => this.fetchYears()}
-                >
-                  Years
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/Venues"
-                  activeClassName="active"
-                  className="link"
-                  data-test="venue-btn"
-                  onClick={() => this.toggleSubNav(true)}
-                >
-                  Venues
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/Songs"
-                  activeClassName="active"
-                  className="link"
-                  data-test="songs-btn"
-                  onClick={() => this.toggleSubNav(true)}
-                >
-                  Songs
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/Tours"
-                  activeClassName="active"
-                  className="link"
-                  data-test="tours-btn"
-                  onClick={() => this.fetchTours()}
-                >
-                  Tours
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/UserStats"
-                  activeClassName="active"
-                  className="link"
-                >
-                  My Stats
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-          {this.state.subNav === true && <SubNav />}
+      <React.Fragment>
+        <header>
+          <div className="container">
+            <nav className="main-nav">
+              <Link
+                to="/"
+                className="link logo-link"
+                data-test="logo-btn"
+                onClick={() => this.toggleSubNav(false)}
+              >
+                <h1 className="logo">
+                  G<span>O</span>PH<span>I</span>SH
+                </h1>
+              </Link>
+              <ul>
+                <li>
+                  <NavLink
+                    to="/BandBio"
+                    activeClassName="active"
+                    className="link"
+                    data-test="band-btn"
+                    onClick={() => this.fetchPhishData()}
+                  >
+                    Band
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Years"
+                    activeClassName="active"
+                    className="link"
+                    data-test="years-btn"
+                    onClick={() => this.fetchYears()}
+                  >
+                    Years
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Venues"
+                    activeClassName="active"
+                    className="link"
+                    data-test="venue-btn"
+                    onClick={() => this.toggleSubNav(true)}
+                  >
+                    Venues
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Songs"
+                    activeClassName="active"
+                    className="link"
+                    data-test="songs-btn"
+                    onClick={() => this.toggleSubNav(true)}
+                  >
+                    Songs
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Tours"
+                    activeClassName="active"
+                    className="link"
+                    data-test="tours-btn"
+                    onClick={() => this.fetchTours()}
+                  >
+                    Tours
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/UserStats"
+                    activeClassName="active"
+                    className="link"
+                  >
+                    My Stats
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            {this.state.subNav === true && <SubNav />}
+          </div>
+        </header>
+        <div className="sidebar-navigation">
+          <div className={sidebarActive ? "sidebarActive sidebar" : "sidebar"}>
+            <section className="nav-header">
+              <div
+                className={sidebarActive ? "iconActive icon" : "icon"}
+                onClick={() => this.toggleSidebar()}
+              >
+                <div className="hamburger" />
+              </div>
+              <Link
+                to="/"
+                className="link logo-link"
+                data-test="logo-btn"
+                onClick={() => this.toggleSubNav(false)}
+              >
+                <h1 className="logo">
+                  G<span>O</span>PH<span>I</span>SH
+                </h1>
+              </Link>
+            </section>
+            <section className="side-nav">
+              <ul className="sidebar-menu">
+                <li>
+                  <NavLink
+                    to="/BandBio"
+                    activeClassName="active"
+                    className="link"
+                    data-test="band-btn"
+                    onClick={() => this.fetchPhishData()}
+                  >
+                    Band
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Years"
+                    activeClassName="active"
+                    className="link"
+                    data-test="years-btn"
+                    onClick={() => this.fetchYears()}
+                  >
+                    Years
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Venues"
+                    activeClassName="active"
+                    className="link"
+                    data-test="venue-btn"
+                    onClick={() => this.toggleSubNav(true)}
+                  >
+                    Venues
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Songs"
+                    activeClassName="active"
+                    className="link"
+                    data-test="songs-btn"
+                    onClick={() => this.toggleSubNav(true)}
+                  >
+                    Songs
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/Tours"
+                    activeClassName="active"
+                    className="link"
+                    data-test="tours-btn"
+                    onClick={() => this.fetchTours()}
+                  >
+                    Tours
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/UserStats"
+                    activeClassName="active"
+                    className="link"
+                    onClick={() => this.toggleSidebar()}
+                  >
+                    My Stats
+                  </NavLink>
+                </li>
+              </ul>
+            </section>
+          </div>
         </div>
-      </header>
+      </React.Fragment>
     );
   }
 }
